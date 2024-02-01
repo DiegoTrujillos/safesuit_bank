c96582405806ea12fb82823c23dbd8a66c3e7847
Deigo <202100184@upqroo.edu.mx>
2024-01-31 20:30:03 -0500

    cartas?

--- lib/core/presentation/widgets/card_widget.dart
+++ lib/core/presentation/widgets/card_widget.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
 class cardWidget extends StatelessWidget{
-  
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      height: 200.0,
+      width: 100.0,
+      padding: const EdgeInsets.all(10.0),
+      child: const Text("hola Mubdo"),);
+  }
+
 }
