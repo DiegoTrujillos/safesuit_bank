4b1f21cb9fa14de90e4d613661f34e5f3c15a556
Deigo <202100184@upqroo.edu.mx>
2024-03-08 13:01:51 -0500

    avatar hecho

--- lib/core/presentation/screers/home.dart
+++ lib/core/presentation/screers/home.dart
@@ -4,13 +4,42 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Inicio")),
-      drawer: NavigationDrawer(children: [
-        Text("Trasferencias"),
-        Text("Pago de servicios"),
-        Text("Movimientos")
-      ]),
-      bottomNavigationBar: BottomNavigationBar(items: [
+      appBar: AppBar(title: const Text("Inicio")),
+      drawer: Drawer(
+        child: ListView(
+          // Importante: Elimina cualquier padding del ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const UserAccountsDrawerHeader(
+              accountName: Text("Nombre del Usuario"), // Aquí pones el nombre del usuario
+              accountEmail: Text("email@example.com"), // Opcional: aquí puedes poner el email del usuario
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage("url_de_la_imagen_del_avatar"), // Pon la URL de la imagen del avatar
+                // Para imágenes locales, puedes usar AssetImage('ruta_de_la_imagen')
+              ),
+            ),
+            ListTile(
+              title: const Text('Transferencias'),
+              onTap: () {
+                // Actualiza la navegación para esta opción
+              },
+            ),
+            ListTile(
+              title: const Text('Pago de servicios'),
+              onTap: () {
+                // Actualiza la navegación para esta opción
+              },
+            ),
+            ListTile(
+              title: const Text('Movimientos'),
+              onTap: () {
+                // Actualiza la navegación para esta opción
+              },
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(items: const [
         BottomNavigationBarItem(
             icon: Icon(
               Icons.home,
@@ -22,19 +51,19 @@ class HomeView extends StatelessWidget {
               Icons.credit_card,
               color: Colors.black,
             ),
-            label: "Credit_Card"),
+            label: "Tarjeta"),
         BottomNavigationBarItem(
             icon: Icon(
               Icons.person,
               color: Colors.black,
             ),
-            label: "Person"),
+            label: "Persona"),
         BottomNavigationBarItem(
             icon: Icon(
               Icons.settings,
               color: Colors.black,
             ),
-            label: "Settings")
+            label: "Ajustes")
       ]),
     );
   }
